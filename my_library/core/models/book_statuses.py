0dc301d2c7fad1937d1215955b33e4d2e9821bbb
lisvv <81390205+lisvv@users.noreply.github.com>
2023-06-15 00:31:30 +0300

    fixtures fix

--- my_library/core/models/book_statuses.py
+++ my_library/core/models/book_statuses.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class BookStatus(models.TextChoices):
-    AVAILABLE = "В наличии"
-    ON_READING = "На прочтении"
-    RESERVED = "Зарезервирована"
+    AVAILABLE = "В наличии", "В наличии"
+    ON_READING = "На прочтении", "На прочтении"
+    RESERVED = "Зарезервирована", "Зарезервирована"
